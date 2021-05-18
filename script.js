e8cef3f7609b76593ee15e51ef8f3fd45677ea7a
hnadrian <hngt.adrian@gmail.com>
2021-05-17 17:37:34 -0700

    Basic functions for calculations

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ nightButton.addEventListener('click', toggleTheme);
 const topScreen = document.querySelector('#top-screen');
 const bottomScreen = document.querySelector('#bottom-screen');
 
-let topScreenValue;
-let bottomScreenValue;
-let previousAns;
-let previousInput;
+let currentCalculation = '';
+let currentOperatorChar = '';
+let currentOperator = '';
+let currentNum1 = '';
+let currentNum2 = '';
+let ans = '';
 
 function start() {
     calButtonns.forEach(button => {
@@ -18,54 +20,87 @@ function start() {
 }
 
 function populateDisplay(topContent, bottomContent) {
-    topScreen.textContent= topContent;
+    topScreen.textContent = topContent;
     bottomScreen.textContent = bottomContent;
 }
 
 function buttonClicked(e) {
-    let button = e.target.id;
-    switch (button) {
+    let buttonID = e.target.id;
+    switch (buttonID) {
         case ('ac'):
             clear();
             break;
         case ('del'):
-            del();
+            
             break;
         case('ans'):
-            populateDisplay(previousInput, previousAns);
             break;
         case('equal'):
+            equal();
             break;
-        case('dot'):
-            break;
-        case('add'):
-            break;
-        case('subtract'):
-            break;
-        case('multiply'):
-            break;
-        case('divide'):
-            break;
-        case('power'):
-            break;
-        case('modulo'):
-            break;
+        default:
+            if (buttonID.startsWith('num')) {
+                let num = buttonID.substring(4);
+                if (num === 'dot') num = '.';
+                bottomScreen.textContent += num;
+                
+                //if the user is entering the value for currentNum1
+                if (currentOperator === '') currentNum1 += num;
+                else currentNum2 += num;
+
+                currentCalculation += num;
+            } else if (buttonID.startsWith('op')) {
+                //if a full calculation was already entered
+                if (currentNum2 != '') {
+                    equal();
+                }
+
+                populateDisplay(bottomScreen.textContent + ' ' + e.target.textContent, '');
+                currentOperator = buttonID.substring(3);
+                currentOperatorChar = e.target.textContent;
+                currentCalculation += ' ' + currentOperatorChar + ' ';
+            }
     }
+}
+
+function equal() {
+    let result = operate(currentOperator,currentNum1,currentNum2);
+    currentCalculation += ' ' + '=';
+    populateDisplay(currentCalculation, result);
+    currentCalculation = result;
+    currentNum2 = '';
+    currentNum1 = result;
+    currentOperator = '';
+    return result;
 }
 
 function operate(operator, num1, num2) {
-    switch (operator) {
-        
-    }
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch(operator) {
+        case 'add':
+            return num1 + num2;
+        case 'subtract':
+            return num1 - num2;
+        case 'multiply':
+            return num1 * num2;
+        case 'divide':
+            return num1 / num2;
+        }
 }
 
 function clear() {
-    console.log('clear');
+    populateDisplay('','');
+    currentCalculation = '';
+    currentOperatorChar = '';
+    currentOperator = '';
+    currentNum1 = '';
+    currentNum2 = '';
+    ans = '';
 }
 
-function del() {
-    let length = bottomScreen.textContent.length;
-    bottomScreen.textContent = bottomScreen.textContent.substring(0, length - 1);
+function clearOp () {
+
 }
 
 function toggleTheme(e) {
